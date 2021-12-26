c7a5c301a8c9e594e511da69770c57f8ce00d39b
putrinurifa <putrinurifa818@gmail.com>
2021-12-26 20:39:54 +0700

    first commit

---
+++ footer.php
@@ -0,0 +1,21 @@
+
+<div class="second-footer">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4">
+            <br>
+            <br>
+            </div>
+            <div class="col-md-4">
+            </div>
+            <div class="col-md-4">
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- Main Footer -->
+<footer class="main-footer text-center">
+    <!-- Default to the left -->
+    <strong>Copyright &copy; 2021 <a href="index.php">TokoHP</a>.</strong> All rights reserved.
+</footer>
